7d794e9a729f4c00cbdbdabc86ca19dcfb9f8309
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-04-21 18:10:21 -0400

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,11 +1,30 @@
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>YouTube Video Downloader | rafled.com</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://static.rafled.com/rafled.com.css">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="robots" content="noindex">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="eS7GuTwHanngzsZY4w0MvIqU1wDZGRjbMSXeihAvxSKnsbYzMQd3ivEzbDS4yb3Z" />
+    
+
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/css/ratings.css">
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/fontawesome/css/all.css">
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/css/theme_default.css" media="all">
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/css/Bootstrap4-callout.css">
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/css/unpoly.min.css">
+    <!-- my-ytapp head code  -->
+    <title>| my-ytapp |</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <link rel="stylesheet" href="style.css">
+   
 <!-- Matomo -->
 <script type="text/javascript">
   var _paq = window._paq = window._paq || [];
@@ -21,16 +40,178 @@
   })();
 </script>
 <!-- End Matomo Code -->
+    <!-- end my-ytapp head code  -->
 
+    <script src="https://filter2.caretech.io/static/js/vendors.js"></script>
+    <script src="https://filter2.caretech.io/static/js/console.js"></script>
+    <script src="https://filter2.caretech.io/static/fontawesome/js/all.js"></script>
+
+     
+     
+
+    <!-- load last for CSS overrides -->
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/css/unpoly-bootstrap3.min.css" media="all">
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/css/unpoly-bootstrap3.min.css" media="all">
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/css/tweaks.css" media="all">
+    <link rel="stylesheet" href="https://filter2.caretech.io/static/css/Bootstrap-callout.css" media="all">
 
 </head>
 <body>
+  <div id="block">
+    <div class="app-container app-theme-white body-tabs-shadow">
+        <div class="app-header header-shadow" style="height: 115px;">
 
-    <h1>YouTube Video Downloader | <a href="https://rafled.com">rafled.com</a></h1>
+            <div class="app-header__content" style="height: 115px;">
+                <div class="app-header-left">
+                    <a href="https://filter2.caretech.io">
+                    <img src="https://filter2.caretech.io/static/images/logo_default.png" width="247" height="94" id="console_home" class="img-responsive mr-2" alt="Security Appliance Logo">
+                    </a>
+                </div> <!-- end of app-header-left -->
+
+                <div class="app-header-right">
+                    <h5 class="pt-2 d-none d-md-block">Video downloading by and for the Plain Community</h5>
+                    <a href="https://filter2.caretech.io/accounts/system/"
+                       class="mr-2 ml-2 btn-transition btn btn-outline-primary">
+                        <i class="fas fa-tachometer-alt" aria-hidden="true"></i>
+                        filter2
+                    </a>
+
+                </div> <!-- end of app-header-right -->
+            </div> <!-- end of app-header__content -->
+        </div> <!-- end of app-header -->
+
+        <div class="app-main">
+                <div class="app-main__outer">
+                    <div class="app-main__inner">
+                        
+
+<div class="card">
+<div class="card-header bg-secondary">
+    Page Content Blocked - www.google.ca
+</div>
+<div class="card-body">
+
+    <div class="card-deck">
+        <div class="card-shadow-warning border card card-body border-warning justify-content-center">
+            <div class="row">
+                <div class="col-4">
+                    <h4>
+                        Category:
+                    </h4>
+                </div>
+                <div class="col-8">
+                  <h4 class="text-warning" id="blockbtn">
+                        Music
+                    </h4>
+                </div>
+            </div>
+            
+                <div class="row">
+                    <div class="col-4">
+                        <h6>
+                            Rating:
+                        </h6>
+                    </div>
+                    <div class="col-8">
+                        <h6 class="text-warning">
+                            <a class="text-warning" href="//filter2.caretech.io/categories/rating/detail/stone/">
+                                Stone
+                            </a>
+                        </h6>
+                    </div>
+                </div>
+            
+            
+                <div class="row">
+                    <div class="col-4">
+                        <h6>
+                            Wrong Category?
+                        </h6>
+                    </div>
+                    <div class="col-8">
+                        <h6>
+                            Select action below
+                        </h6>
+                    </div>
+                </div>
+            
+        </div>
+        <div class="card-shadow-warning border card card-body border-warning d-none d-sm-block">
+            <p>
+                This category is blocked in your profile. If your profile needs to be changed,
+                contact your system administrator, or login to the Log Cabin console
+                to make the necessary changes.
+            </p>
+        </div>
+    </div>
+
+    
+
+    <div class="d-none">
+        
+        <form id="blockpage_data" method="post" action="https://filter2.caretech.io/overrides/blockpage/safe/">
+            <input name="bp_data" type="hidden" value='{"categories":["Music"],"method":"GET","rule":{"Action":"block","Needed":["content_music"],"Disallowed":null,"Description":""},"scores":{"art":-510,"audioelectronics":60,"auto":25,"bad_words":35,"celebrity":80,"computer":92,"content_movies":-250,"content_music":545,"dating":90,"education":-150,"humor":125,"hunting":-105,"isptelecom":6,"lc_Always_Block_298":1500,"lc_classic_furnishings_email_only":1500,"lc_matchall":1500,"lc_matchallNone":1500,"life_events":40,"news/news_celebrity":80,"news/news_humor":125,"nudism":110,"popculture":44,"pornography":45,"publishing":25,"sacred_music":195,"searchengines":200,"socialnetworking":60,"swimming":-15,"wash_language":5500},"tally":{"/./":1,"/google.{0,10}search/":1,"< dance >":2,"< discograph>":2,"< ipad >":1,"< lol >":5,"< lyrics>":3,"< shit >":1,"< songs >":15,"< trailer>":1,"<dance music>":2,"<digital audio workstation>":2,"<dream girl>":2,"<dsl>":2,"<function call >":2,"<hunter st>":7,"<lady gaga>":2,"<live performan>":1,"<macintosh>":1,"<miss you>":2,"<muievd>":1,"<music artist>":1,"<musical style>":1,"<radial>":1,"<regular expression>":1,"<standard color>":1,"google.ca":1},"url":"https://www.google.ca/search?client=safari&hl=en-ca&ie=UTF-8&oe=UTF-8&q=basshunter+music&safe=vss","user":"willy_klassen","category_reason":"Music","hide_action_btn":false,"css_class":"warning","bp_panel_title":"Page Content Blocked","rating":"stone","display_action_prompt":true,"User-Agent":["BackgroundShortcutRunner/1092.8.7 CFNetwork/1220.1 Darwin/20.3.0"],"conditions":["content_music"],"domain_name":"www.google.ca"}' />
+            <input name="csrfmiddlewaretoken" type="hidden" value="eS7GuTwHanngzsZY4w0MvIqU1wDZGRjbMSXeihAvxSKnsbYzMQd3ivEzbDS4yb3Z"/>
+            <input type="submit" value="submit" class="d-none"/>
+        </form>
+    </div>
+
+    
+    <div class="col-sm-12 col-md-8 col-lg-6 col-xl-4 col-centered mt-3 mb-3">
+        <div class="card">
+            <div class="card-body">
+                <a href="#" onclick="document.getElementById('blockpage_data').submit(); return false;"
+                   class="btn-transition btn btn-outline-primary btn-block">
+                    <i class="fas fa-key btn-icon-wrapper btn-icon-lg"> </i><br/>
+                    <b>Help Me Access This Page</b>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    
+
+</div>
+</div>
+
+
+
+
+                    </div> <!-- end of app-main__inner -->
+                    <div class="app-wrapper-footer">
+                        <div class="app-footer">
+                            <div class="app-footer__inner">
+                                <div class="app-footer-left">
+                                    <h5>
+                                        Thank you for being a Security Appliance user
+                                    </h5>
+                                </div> <!-- end of app-footer-left -->
+                                <div class="app-footer-right">
+                                    <a href="https://filter2.caretech.io/carecenter/ticket/list/"
+                                       class="mr-2 ml-2 btn-transition btn btn-lg btn-outline-primary">
+                                        <i class="fa fa-life-ring" aria-hidden="true"></i>
+                                        Care Center
+                                    </a>
+                                </div> <!-- end of app-footer-right -->
+                            </div> <!-- end of app-footer__inner -->
+                        </div> <!-- end of app-footer -->
+                    </div> <!-- end of app-wrapper-footer -->
+                </div> <!-- end of app-main__outer -->
+            </div> <!-- end of app-main -->
+        </div> <!-- end of app-container -->
+
+    </div>
+  <div id="download" style="display:none;">
+   
+      <br>
+  <br>
+  <br>
+  <br>
+    <h1 id="downloadbtn">my-ytapp |</h1><h1> <a href="https://rafled.com">rafled.com</a></h1>
  <center>
 <form action="https://api.rafled.com/youtube-downloader/v1/">
-    <input class="input-res" type="text" name="url" placeholder="paste video url here" size="80" id="txt_url" required/>
-    <input class="button" type="button" id="btn_fetch" value="Download Video" onclick="unhidelol()">
+  <input class="input-res" type="text" name="url" placeholder="paste video url here" size="80" id="txt_url" required/><br>
+    <input class="button" type="button" id="btn_fetch" value="Download" onclick="unhidelol()">
     <input class="button" type="submit" value="JSON API">
      </form>
 <div style="display:none;" id="hiddenlol">
@@ -41,47 +222,31 @@
     </center>
    </div>
     <hr>
-    <p> [Early Access] Now you can choose Video Quality and Audio quality when downloading, check it out <a href="https://rafled.com/beta/ytd.php">here</a>.</p>
-    <hr>
-    <p><b> Please Note:</b> This program is for personal use only. Downloading copyrighted material without permission is against <a href="https://www.youtube.com/static?template=terms">YouTube's terms of services</a>. By using this program, you are solely responsible for any copyright violations. We are not responsible for people who attempt to use this program in any way that breaks YouTube's terms of services.
-    </p>
-<script type="text/javascript">
-    function unhidelol() {
-   document.getElementById('hiddenlol').style.display = "block";
-}
-    </script>
-<script>
-    $(function () {
-        $("#btn_fetch").click(function () {
-            var url = $("#txt_url").val();
-            var oThis = $(this);
-            oThis.attr('disabled', true);
-            $.get('video_info.php', {url: url}, function (data) {
-                console.log(data);
-                oThis.attr('disabled', false);
-                var links = data['links'];
-                var error = data['error'];
-                if (error) {
-                    alert('Error: ' + error);
-                    return;
-                }
-                // first link with video
-                var first = links.find(function (link) {
-                    return link['format'].indexOf('video') !== -1;
-                });
-                if (typeof first === 'undefined') {
-                    alert('No video found!');
-                    return;
-                }
-                var stream_url = 'stream.php?url=' + encodeURIComponent(first['url']);
-                var video = $("video");
-                video.attr('src', stream_url);
-                video[0].load();
-            });
-        });
-    });
-</script>
-
-</body>
+  <hr>
+  </div> 
+  <script type="text/javascript">
+    window.addEventListener("DOMContentLoaded", function() {
+  var block = document.getElementById("block");
+  var download = document.getElementById("download");
+  var blockbtn = document.getElementById("blockbtn");
+  var downloadbtn = document.getElementById("downloadbtn");
+      blockbtn.addEventListener("dblclick",function(ev) {
+  ev.preventDefault();
+  block.style = "display:none";
+  download.style="display:unset;";
+  });
+      
+downloadbtn.addEventListener("dblclick",function(ev) {
+  ev.preventDefault();
+  download.style = "display:none";
+  block.style="display:unset;";
+  });
+});
+  
+ 
+    
+    
+   
+  </script>
+  </body>
 </html>
-
