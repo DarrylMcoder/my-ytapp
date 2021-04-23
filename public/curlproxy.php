1f9e9523412c07c9fbd7309f336f4cca005598a2
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-04-23 06:00:19 -0400

    Add files via upload

---
+++ public/curlproxy.php
@@ -0,0 +1,16 @@
+<?php
+
+function lookup(){  
+$statica_env = getenv("QUOTAGUARD_URL");      $statica = parse_url($statica_env);  $proxyUrl $statica['host'].":".$statica['port'];  $proxyAuth = $statica['user'].":".$statica['pass'];  
+$url = "http://ip.jsontest.com/";  
+$ch = curl_init();  
+curl_setopt($ch, CURLOPT_URL, $url);  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  curl_setopt($ch, CURLOPT_PROXY, $proxyUrl);  curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC);  
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyAuth);  
+$response = curl_exec($ch);  
+return $response;
+}
+
+$res = lookup();
+print_r($res);
+
+?>
