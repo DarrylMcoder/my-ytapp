cb3aafeb1b7f14b47db5e3190adb94f04730372b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-04-23 09:42:03 -0400

    Add files via upload

---
+++ public/search_video.php
@@ -0,0 +1,20 @@
+<?PHP
+    
+require('../vendor/autoload.php');
+
+$query = isset($_GET['q']) ? $_GET['q'] : null;
+
+if (!$query) {
+    die("No search query provided");
+}
+
+
+$youtube = new \YouTube\Search();
+$links = $youtube->search_links($query);
+
+header('Content-Type: application/json');
+echo json_encode([
+    'links' => $links,
+], JSON_PRETTY_PRINT);
+    
+?>
