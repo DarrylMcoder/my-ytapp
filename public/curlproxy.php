85aa1a0e59811d3fa76b033521d207f55bcc9712
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-04-23 06:08:34 -0400

    Update curlproxy.php

--- public/curlproxy.php
+++ public/curlproxy.php
@@ -2,7 +2,7 @@
 
 function lookup(){  
 $statica_env = getenv("QUOTAGUARD_URL");      $statica = parse_url($statica_env);  $proxyUrl $statica['host'].":".$statica['port'];  $proxyAuth = $statica['user'].":".$statica['pass'];  
-$url = "http://ip.jsontest.com/";  
+$url = "http://www.google.com";  
 $ch = curl_init();  
 curl_setopt($ch, CURLOPT_URL, $url);  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  curl_setopt($ch, CURLOPT_PROXY, $proxyUrl);  curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC);  
 curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyAuth);  
