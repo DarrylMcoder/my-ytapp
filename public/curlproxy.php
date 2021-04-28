fe6fc6f8722962aa9abccbb42f11fb89d3c5046a
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-04-28 05:56:39 -0400

    Update curlproxy.php

--- public/curlproxy.php
+++ public/curlproxy.php
@@ -1,16 +1,22 @@
 <?php
+$url = $_GET['url'];
 
-function lookup(){  
-$statica_env = getenv("QUOTAGUARD_URL");      $statica = parse_url($statica_env);  $proxyUrl $statica['host'].":".$statica['port'];  $proxyAuth = $statica['user'].":".$statica['pass'];  
-$url = "http://www.google.com";  
-$ch = curl_init();  
-curl_setopt($ch, CURLOPT_URL, $url);  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  curl_setopt($ch, CURLOPT_PROXY, $proxyUrl);  curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC);  
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyAuth);  
-$response = curl_exec($ch);  
-return $response;
-}
+  function proxyRequest($url) {
+    $fixieUrl = getenv("QUOTAGUARD_URL");
+    $parsedFixieUrl = parse_url($fixieUrl);
 
-$res = lookup();
-print_r($res);
+    $proxy = $parsedFixieUrl['host'].":".$parsedFixieUrl['port'];
+    $proxyAuth = $parsedFixieUrl['user'].":".$parsedFixieUrl['pass'];
 
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyAuth);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+  }
+
+  $response = proxyRequest($url);
+  print_r($response);
 ?>
