18ddd3268c89da3f14b3171523d3746c7b0549dd
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-04-28 21:47:45 -0400

    Add files via upload

--- public/video_info.php
+++ public/video_info.php
@@ -18,9 +18,12 @@ $error = $youtube->getLastError();
 
 $info = $youtube->getInfo();
 
+$name = $youtube->getVideoName();
+
 header('Content-Type: application/json');
 echo json_encode([
     'links' => $links,
     'error' => $error,
-    'info' => $info
+    'info' => $info,
+    'name' => $name
 ], JSON_PRETTY_PRINT);
