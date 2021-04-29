eb619e39207ace2c0f8bd608eba284cda8e91426
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-04-28 21:45:28 -0400

    Add files via upload

--- public/video_info.php
+++ public/video_info.php
@@ -8,12 +8,15 @@ if (!$url) {
     die("No url provided");
 }
 
+//$url = base64_decode($url);
+
 $youtube = new \YouTube\YouTubeDownloader();
+
 $links = $youtube->getDownloadLinks($url);
 
 $error = $youtube->getLastError();
 
-$info = $youtube->info;
+$info = $youtube->getInfo();
 
 header('Content-Type: application/json');
 echo json_encode([
