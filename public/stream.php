09f25253010209d580dd2f6afd396b6fcc54bf80
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-05-01 11:14:13 -0400

    Add files via upload

--- public/stream.php
+++ public/stream.php
@@ -10,8 +10,11 @@ if ($url == false) {
     die("No url provided");
 }
 
+$debug = isset($_GET['debug']) ? $_GET['debug'] : false;
+
 //comment next line to disable encryption
 $url = base64_decode($url);
 
 $youtube = new \YouTube\YoutubeStreamer();
+$youtube->debug = $debug;
 $youtube->stream($url);
