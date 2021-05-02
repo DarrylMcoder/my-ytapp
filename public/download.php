a848429cebc31580b4c34e3651556bd540f16009
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-05-02 07:00:00 -0400

    Add files via upload

--- public/download.php
+++ public/download.php
@@ -10,8 +10,6 @@ if ($url == false) {
     die("No url provided");
 }
 
-$youtubefiledownloader = new \YouTube\YouTubeFileDownloader();
-
 $youtube = new \YouTube\YouTubeDownloader();
 
 $links = $youtube->getDownloadLinks($url);
@@ -23,6 +21,18 @@ $info = $youtube->getInfo();
 $name = $youtube->getVideoName();
 
 $vid_url = $links[0]['url'];
+
+if(!isset($vid_url)){
+die "No video found!";
+}
+
+if(isset($error)){
+  die "ERROR: ".$error;
+}
+
+$youtubefiledownloader = new \YouTube\YouTubeFileDownloader();
+
+$youtubefiledownloader->setFileName($name);
 
 $youtubefiledownloader->download($vid_url);
     
