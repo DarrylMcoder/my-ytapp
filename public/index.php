e8f3c90e2e66eed3d90668381399d00ca211cdf9
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-05-03 07:51:32 -0400

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -10,6 +10,29 @@ $url = isset($_GET['url']) ? $_GET['url'] : null;
     <link rel="stylesheet" href="https://static.rafled.com/rafled.com.css">
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  
+  <!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.my-ytapp.herokuapp.com"]);
+  _paq.push(["setDomains", ["*.my-ytapp.herokuapp.com"]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://my-ytapp.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/my-ytapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="https://my-ytapp.matomo.cloud/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
+  
+  
 </head>
 <body>
   <br>
