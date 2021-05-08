66146de13b8e70d4608547935e8ccde825465d1d
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-05-08 14:59:17 -0400

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -18,11 +18,11 @@ $url = isset($_GET['url']) ? $_GET['url'] : null;
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//analytics.darrylmcoder.com/";
+    var u="https://darrylmcoder.matomo.cloud/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/darrylmcoder.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
 <!-- End Matomo Code -->
